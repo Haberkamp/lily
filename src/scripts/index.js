bfe5b00d7120f69998dc2af73f65d4e07ca2be31
Nils Haberkamp <n.haberkamp@shopware.com>
2020-12-26 19:30:42 +0100

    add footer

---
+++ src/scripts/index.js
@@ -0,0 +1,7 @@
+window.boostrap = require('./vendors/bootstrap.bundle.min');
+
+// enabling tooltips everywhere
+const tooltipTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="tooltip"]'))
+tooltipTriggerList.forEach(tooltipTriggerEl => {
+  return new window.boostrap.Tooltip(tooltipTriggerEl)
+});
