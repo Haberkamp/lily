dbdbecd4f3bde74ea9dfcc35ad0c92d9179935c1
Nils Haberkamp <n.haberkamp@shopware.com>
2020-12-26 19:42:09 +0100

    run prettier

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,7 +1,9 @@
 window.boostrap = require('./vendors/bootstrap.bundle.min');
 
 // enabling tooltips everywhere
-const tooltipTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="tooltip"]'))
+const tooltipTriggerList = [].slice.call(
+  document.querySelectorAll('[data-bs-toggle="tooltip"]')
+);
 tooltipTriggerList.forEach(tooltipTriggerEl => {
-  return new window.boostrap.Tooltip(tooltipTriggerEl)
+  return new window.boostrap.Tooltip(tooltipTriggerEl);
 });
